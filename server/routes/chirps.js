949cf8bf989bb92999e69e8b8a65ca9dd47f7a33
mendi <mendenhall_chris1246@yahoo.com>
2023-09-15 17:40:26 -0700

    GET POST PUT and DELETE methods are working

--- server/routes/chirps.js
+++ server/routes/chirps.js
@@ -1,8 +1,31 @@
 const express = require("express");
+const chirps = require("../chirpstore");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.send("chirps");
+router.get("/:id?", (req, res) => {
+	const id = req.params.id;
+	if (id) {
+		res.send(chirps.GetChirp(id));
+	} else {
+		res.send(chirps.GetChirps());
+	}
+});
+
+router.post("/", (req, res) => {
+	chirps.CreateChirp(req.body);
+	res.sendStatus(200);
+});
+
+router.put("/:id?", (req, res) => {
+	const id = req.params.id;
+	chirps.UpdateChirp(id, req.body);
+	res.sendStatus(200);
+});
+
+router.delete("/:id?", (req, res) => {
+	const id = req.params.id;
+	chirps.DeleteChirp(id);
+	res.sendStatus(200);
 });
 
 module.exports = router;
